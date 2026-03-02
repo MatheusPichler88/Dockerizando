59e663e143f1c43d1cccc3930faffb7c501d3798
MatheusPichler88 <matheuspichler94@gmail.com>
2026-03-02 17:05:24 -0300

    feat: inicializando a aplicação

---
+++ docker/src/main/java/dev/java10x/DockerAulaJava10x/DockerAulaJava10xApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.DockerAulaJava10x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAulaJava10xApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAulaJava10xApplication.class, args);
+	}
+
+}
---
+++ docker/src/test/java/dev/java10x/DockerAulaJava10x/DockerAulaJava10xApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.DockerAulaJava10x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAulaJava10xApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
